ff822ddeea2747e3df963b875df849a360c2f65d
ignjaticaco <aleksandar.ignjatic99@gmail.com>
2017-06-08 12:45:55 +0200

    Create video_insert.php

--- video_insert.php
+++ video_insert.php
@@ -1,1 +1,10 @@
+<?php
+require 'database.php';
 
+  if((isset)POST ['url']){
+    $url = $_POST ['url'];
+    $url = explode ['=',$url];
+    $ur = $url ['1'];
+    
+    <iframe widht="560" height = "315" src = "http://youtube.com/embed/<?php echo $url; ?> frameboarder="0" allowfullscreen"
+  }
