8230a58798177be470bfae195b212ab16a5a9ca8
ignjaticaco <aleksandar.ignjatic99@gmail.com>
2017-06-08 13:17:30 +0200

    Create video_insert.php

--- video_insert.php
+++ video_insert.php
@@ -1,6 +1,18 @@
 <?php
 require 'database.php';
 
+include_once 'database.php';
+        
+        $query = sprintf("INSERT INTO videos(url, title, destionation_id)
+                  VALUES ('%s','%s',$id)",
+                 mysqli_real_escape_string($link, $target_file),
+                 mysqli_real_escape_string($link, $title));
+        //echo $query; die();
+        mysqli_query($link,$query);
+        $_SESSION['success'] = 'Video je uspešno naložen.';
+        header("Location: destination.php?id=$id");
+        die();
+
   if((isset)POST ['url']){
     $url = $_POST ['url'];
     $url = explode ['=',$url];
